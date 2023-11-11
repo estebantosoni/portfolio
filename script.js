b64a16945539493217133130f88699e712541e4c
estebantosoni <tosoniesteban@gmail.com>
2023-11-11 10:13:46 -0300

    fix errors & optimize code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+    const menuLinks = document.querySelector(".menu-links");
+
+    hamburgerIcon.addEventListener("click", toggleMenu);
+
+    function toggleMenu() {
+        menuLinks.classList.toggle("open");
+        hamburgerIcon.classList.toggle("open");
+    }
+});
